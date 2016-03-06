66914494fe86e0cf6fc36f49015964013cdfd885
Garrett Levine <garrlevine@gmail.com>
2016-03-06 16:37:02 -0500

    completed bare minimum of the site

--- page.php
+++ page.php
@@ -9,13 +9,59 @@
           </div>
 
           <div class="page__wrapper">
-            <div class="page__row">
+            <div class="page__pageContent">
   
           <?php the_content(); ?>
   
         <?php endwhile; // end the loop?>
-      </div> <!-- /,content -->
-    </div> <!-- /.container -->
+            </div> <!-- /.row-->
+            <div class="page__members">
+              <div class="page__memberCard">
+                <div class="memberCard__imageContainer">
+                  <?php 
+                    $memberImage1 = get_field('member_photo_1');
+                    if( !empty($memberImage1) ): ?>
+                    <img src="<?php echo $memberImage1['url']; ?>" alt="<?php echo $memberImage1['alt']; ?>" />
+                  <?php endif; ?>
+                </div>
+                <div class="memberCard__textContainer">
+                  <h4 class="memberCard__name"><?php the_field('member_name_1') ?></h4>
+                  <h5 class="memberCard__title"><?php the_field('member_title_1') ?></h5>
+                  <p class="memberCard__description"><?php the_field('member_description_1') ?></p>
+                </div>
+              </div>
+
+              <div class="page__memberCard">
+                <div class="memberCard__imageContainer">
+                  <?php 
+                    $memberImage2 = get_field('member_photo_2');
+                    if( !empty($memberImage2) ): ?>
+                    <img src="<?php echo $memberImage2['url']; ?>" alt="<?php echo $memberImage2['alt']; ?>" />
+                  <?php endif; ?>
+                </div>
+                <div class="memberCard__textContainer">
+                  <h4 class="memberCard__name"><?php the_field('member_name_2') ?></h4>
+                  <h5 class="memberCard__title"><?php the_field('member_title_2') ?></h5>
+                  <p class="memberCard__description"><?php the_field('member_description_2') ?></p>
+                </div>
+              </div>
+
+              <div class="page__memberCard">
+                <div class="memberCard__imageContainer">
+                  <?php 
+                    $memberImage3 = get_field('member_photo_3');
+                    if( !empty($memberImage3) ): ?>
+                    <img src="<?php echo $memberImage3['url']; ?>" alt="<?php echo $memberImage3['alt']; ?>" />
+                  <?php endif; ?>
+                </div>
+                <div class="memberCard__textContainer">
+                  <h4 class="memberCard__name"><?php the_field('member_name_3') ?></h4>
+                  <h5 class="memberCard__title"><?php the_field('member_title_3') ?></h5>
+                  <p class="memberCard__description"><?php the_field('member_description_3') ?></p>
+                </div>
+              </div>
+            </div>
+    </div> <!-- /.wrapper-->
   </div> <!-- /.main -->
   
   <?php get_footer(); ?>
